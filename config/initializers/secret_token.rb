fbf3ea04fd5e1216a31c16891f8b341fc9698075
dave <me@daveczarnecki.com>
2013-07-30 01:42:02 +0000

    Initial Repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ShedSpread::Application.config.secret_token = '4b622f6b527d387b801af8cb605bf200cd76332299209c3e42ae9e632b9a69268e9becd3bb6504b45fbb16dffb4170e13c306211e504b904b98c58ac6e158ae2'
